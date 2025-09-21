ae7d84a0d8c47e25115056c2ea555652a14191e5
Nikolya Khachatryan <164537426+Nikolya21@users.noreply.github.com>
2025-09-21 18:06:39 +0300

    feat: add Human model and classes for third_homework

---
+++ src/main/java/com/mipt/nikolyakhachatryan/Working_person.java
@@ -0,0 +1,8 @@
+package com.mipt.nikolyakhachatryan;
+
+public abstract class Working_person {
+    public abstract void(int hours);
+    public boolean goHome(String string1, String string2) {
+        return string1.equals(string2);
+    }
+}
