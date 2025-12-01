a3a1dd8e0ffe852436f409d8daca7e8784c564a5
Nikolya Khachatryan <164537426+Nikolya21@users.noreply.github.com>
2025-12-01 09:07:16 +0300

    Done homework

---
+++ src/main/java/com/mipt/nikolyakhachatryan/bankAccount2/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package com.mipt.nikolyakhachatryan.bankAccount2;
+
+public class InsufficientFundsException extends RuntimeException {
+  public InsufficientFundsException(String message) {
+    super(message);
+  }
+}
